0557c523824894f69239eb2a523c1d01dec6946a
ERCOPII <miguelangelealdelojos@gmail.com>
2024-10-30 11:10:23 +0100

    Ejercicio Completado

--- src/app/DrinkList.js
+++ src/app/DrinkList.js
@@ -1,14 +1,27 @@
 function Drink({ name }) {
+
+  let partePlanta, contenidoCafeina, antiguedad;
+
+  if (name === 'té') {
+    partePlanta = 'hoja';
+    contenidoCafeina = '15/70 mg/taza';
+    antiguedad = '4,000+ años';
+  } else {
+    partePlanta = 'grano';
+    contenidoCafeina = '80-185 mg/taza';
+    antiguedad = '1,000+ años';
+  }
+
     return (
       <section>
         <h1>{name}</h1>
         <dl>
           <dt>Parte de la planta</dt>
-          <dd>{name === 'té' ? 'hoja' : 'grano'}</dd>
+          <dd>{partePlanta}</dd>
           <dt>Contenido de cafeína</dt>
-          <dd>{name === 'té' ? '15–70 mg/taza' : '80–185 mg/taza'}</dd>
+          <dd>{contenidoCafeina}</dd>
           <dt>Antigüedad</dt>
-          <dd>{name === 'té' ? '4,000+ años' : '1,000+ años'}</dd>
+          <dd>{antiguedad}</dd>
         </dl>
       </section>
     );
